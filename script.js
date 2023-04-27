608b36d93a73cc9eb32480684a4635e7e0f26be2
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-04-27 09:23:11 +0200

    ny log

--- script.js
+++ script.js
@@ -3,10 +3,15 @@
 window.addEventListener("load", start);
 
 function start() {
-    console.log("js is on..");
-    endnuEnLog()
+  console.log("js is on..");
+  endnuEnLog();
 }
 
 function endnuEnLog(params) {
-    console.log("loggies!!!")
+  console.log("loggies!!! nu med karamel smag");
+  endnuEndnuEnLog();
+}
+
+function endnuEndnuEnLog() {
+  console.log("b logger");
 }
